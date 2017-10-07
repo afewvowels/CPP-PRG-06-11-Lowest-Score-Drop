2a0f813c4e128cce4c14c978b0a5b30345f0c1d3
Keith Smith <aeiouandsometimesy@me.com>
2017-10-07 19:46:56 -0400

    Initial Commit

---
+++ PRG-6-11-Lowest-Score-Drop/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PRG-6-11-Lowest-Score-Drop
+//
+//  Created by Keith Smith on 10/7/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
